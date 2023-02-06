07834eec70d656d9e1782359b409a44e19dd052f
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-06 18:11:37 +0800

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,3 +1,7 @@
 # Queue class Code Source: https://realpython.com/queue-in-python/#implementing-queues-in-python
 from collections import deque
 from heapq import heappop, heappush
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
