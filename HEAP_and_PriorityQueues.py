5f72a0b67bf6499a1b6d3419d193b4e60d6f5eac
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-07 05:47:49 +0800

    heap push

--- HEAP_and_PriorityQueues.py
+++ HEAP_and_PriorityQueues.py
@@ -2,3 +2,5 @@ from queue import PriorityQueue, ReversedPriorityQueue, heappush, heappop
 
 # Heap testing 1, using heappush and heappop from heapq module 
 languages = []
+heappush(languages, "Mandarin") #2 in alphabetical order
+heappush(languages, "English")  #1 in alphabetical order
