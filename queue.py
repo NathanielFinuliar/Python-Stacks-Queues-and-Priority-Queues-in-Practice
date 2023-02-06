6f8b19264854a2130de906ae97b5e1ff69503ef0
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-06 18:46:57 +0800

    Update queue.py

--- queue.py
+++ queue.py
@@ -5,3 +5,7 @@ from heapq import heappop, heappush
 class Queue:
     def __init__(self, *elements):
         self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
