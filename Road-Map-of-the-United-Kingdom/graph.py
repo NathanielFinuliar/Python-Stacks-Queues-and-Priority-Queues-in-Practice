1f1a0712520cf293f3d88650a7f28482101d4a5a
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-08 03:27:25 +0800

    added city class

--- Road-Map-of-the-United-Kingdom/graph.py
+++ Road-Map-of-the-United-Kingdom/graph.py
@@ -4,4 +4,14 @@
 from collections import deque
 from math import inf as infinity
 from typing import NamedTuple
+
 import networkx as nx
+from queue import MutableMinHeap, Queue, Stack
+
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
