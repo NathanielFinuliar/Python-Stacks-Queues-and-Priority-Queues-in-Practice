0735f48d5dbb0f41a7fddc1dc1c4c96fc107fb7b
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-08 03:30:49 +0800

    added iterable class

--- Road-Map-of-the-United-Kingdom/queue.py
+++ Road-Map-of-the-United-Kingdom/queue.py
@@ -7,3 +7,10 @@ from heapq import heapify, heappop, heappush
 from itertools import count
 from typing import Any
 
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
