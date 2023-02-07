1223b1e59db2e062a38bdd8f125ead21d624ad7c
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-08 04:28:29 +0800

    added breadth search function

--- Road-Map-of-the-United-Kingdom/graph.py
+++ Road-Map-of-the-United-Kingdom/graph.py
@@ -51,3 +51,7 @@ def breadth_first_traverse(graph, source, order_by=None):
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate, order_by=None):
+    return search(breadth_first_traverse, graph, source, predicate, order_by)
+
