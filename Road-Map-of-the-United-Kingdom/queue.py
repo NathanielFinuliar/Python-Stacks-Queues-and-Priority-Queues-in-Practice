624798580e65549a164139ba9aee67f4a597f492
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-08 04:29:48 +0800

    added Queue class

--- Road-Map-of-the-United-Kingdom/queue.py
+++ Road-Map-of-the-United-Kingdom/queue.py
@@ -7,6 +7,7 @@ from heapq import heapify, heappop, heappush
 from itertools import count
 from typing import Any
 
+
 class IterableMixin:
     def __len__(self):
         return len(self._elements)
@@ -14,3 +15,14 @@ class IterableMixin:
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
+
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
