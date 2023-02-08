49584ae3266e47f1659e9487c19327bff54fd1a2
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-08 19:37:38 +0800

    added dequeue function

--- Road-Map-of-the-United-Kingdom/queue.py
+++ Road-Map-of-the-United-Kingdom/queue.py
@@ -41,3 +41,6 @@ class PriorityQueue(IterableMixin):
     def enqueue_with_priority(self, priority, value):
         element = (-priority, next(self._counter), value)
         heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
